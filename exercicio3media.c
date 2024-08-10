4bcdafa152a970b4b8ec8188e7e052d6edf262cc
Guilherme <168233537+GuilhermeFideliscch@users.noreply.github.com>
2024-08-09 22:18:42 -0300

    Add files via upload

---
+++ exercicio3media.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    float n1,n2,n3,n4,r;
+    char escolha;
+    printf("escolha o tipo de media:\nA-aritmetica\nP-ponderada\nH-harmonica\n");
+	scanf("%c",& escolha);
+	printf("nota 1: ");
+	scanf("%f",& n1);    							
+	return 0;
+}
