ccb5c815118be25fb6dffa06be77f728c1a6fd04
Guilherme <168233537+GuilhermeFideliscch@users.noreply.github.com>
2024-08-09 22:22:13 -0300

    Update exercicio3media.c

--- exercicio3media.c
+++ exercicio3media.c
@@ -5,6 +5,15 @@ int main(){
     printf("escolha o tipo de media:\nA-aritmetica\nP-ponderada\nH-harmonica\n");
 	scanf("%c",& escolha);
 	printf("nota 1: ");
-	scanf("%f",& n1);    							
+	scanf("%f",& n1);
+	printf("nota 2: ");
+	scanf("%f",& n2);
+	printf("nota 3: ");
+	scanf("%f",& n3);
+	printf("nota 4: ");
+	scanf("%f",& n4);
+	if(escolha == 'A'){
+		
+	}          							
 	return 0;
 }
